5723f24ce7225157f73ef4b90b53e7eb6058d1d3
Zaida Hammel <zzaida.hammel@mths.ca>
2022-09-15 15:11:18 +0000

    added cpp hellow world file

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Zaida Hammel All rights reserved
+//
+// Created by: Zaida Hammel
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+    std::cout << "\nDone.\n";
+}
